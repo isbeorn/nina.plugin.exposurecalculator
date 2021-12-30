dbe4dde60bd449d7e130ddb549e7a4690266f271
Stefan Berg <isbeorn86@googlemail.com>
2021-12-30 22:06:16 +0100

    Add description that was previously in nina docs

--- NINA.Plugin.ExposureCalculator/Properties/AssemblyInfo.cs
+++ NINA.Plugin.ExposureCalculator/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
 
 //The minimum Version of N.I.N.A. that this plugin is compatible with
 [assembly: AssemblyMetadata("MinimumApplicationVersion", "2.0.0.2020")]
@@ -58,6 +58,33 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ScreenshotURL", "")]
 //An additional example screenshot of your plugin in action
 [assembly: AssemblyMetadata("AltScreenshotURL", "")]
-[assembly: AssemblyMetadata("LongDescription", @"This tool will suggest a recommended exposure time based on the read noise from your sensor and the average skyglow.
-If you have SharpCap Pro installed and perform a Sensor Analysis, then use the drop down below to retrieve the read noise and full well capacity for the specified gain.
-You can also calculate the bias by clicking the Snapshot button next to Bias Mean.")]
+[assembly: AssemblyMetadata("LongDescription", @"
+This tool will suggest a recommended exposure time based on the read noise from the camera sensor and the average skyglow.
+If SharpCap is installed and a Sensor Analysis is available for the current camera, RN and FW are derived from the sensor analysis.
+
+1. Exposure time. This is only used to measure the average skyglow when clicking on (8)
+2. Filter: this menu lets you choose the filter for the calculation
+    > Filters affect the  wavelength bandpass of incident light and therefore the average skyglow. The analysis should be repeated for each filter to determine an optimal exposures set.
+3. Gain: select the gain for the exposure analysis. 
+    > Camera parameters vary significantly with gain values, the analysis should be repeated for the different gain values used for imaging. A guideline to determine the optimal gain values for your imaging conditions can be found [here](https://www.youtube.com/watch?v=ub1HjvlCJ5Y&list=PLhIb8N-jSR_rNKxCFGzbd87TfmyQS4U4X&index=14)
+4. The drop-down menu lets you select available sensor analysis files from SharpCap
+   > SharpCap must be installed and you must first perform a Sensor Analysis in SharpCap following the instructions [here](http://docs.sharpcap.co.uk/3.2/19_SensorAnalysis.htm). Sensor analysis files are saved in %APPDATA%\Roaming\SharpCap\SensorCharacteristics
+5. Full Well Capacity in electrons: if known this value can be entered manually for the specified gain or retrieved automatically from the Sharpcap Sensor Analysis
+6. Read Noise in electrons: if known this value can be entered manually for the specified gain or retrieved automatically from the Sharpcap Sensor Analysis
+7. BIAS median value (in 16bit): median ADU value of a bias frame (scald to 16bit), can be entered manually or calculated automatically by covering the scope and clicking on the 'Calculate Bias' button
+8.Click here to perform the exposure for the analysis
+9.The recommended exposure times are displayed in this section
+
+Recommended exposure time is calculated according to the following[formula](https://forums.sharpcap.co.uk/viewtopic.php?t=456):
+
+`Recommended Exposure Time = 10 * read noise squared / light pollution rate`
+
+where light pollution rate is defined as:
+
+`(median ADU of a subframe - median of the bias) * electrons per ADU / the length of the exposure`
+
+Further details on the theory for the optimal exposure calculation can be found [here](https://www.youtube.com/watch?v=3RH93UvP358)
+
+ > The analysis will use whatever gain is specified and linear interpolate between the values calculated by sharpcap. for example, if you have read noise in the analysis for gains 100 and 150 but specify 125, the tool will set the read noise exactly between the two.
+
+ > Remember to cool down your camera to the desired temperature before using the tool, high Dark Current values may affect the results.")]
